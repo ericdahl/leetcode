e812384f3a78d2e035a8bc668d7f8c0c1ded86a9
Eric Dahl <eric.c.dahl@gmail.com>
2022-08-13 13:28:42 -0700

    python: add 82

---
+++ python3/p082.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    @staticmethod
+    def ll_to_list(head: Optional[ListNode]) -> list:
+
+        l = []
+        curr = head
+        while curr:
+            l.append(curr.val)
+            curr = curr.next
+        return l
+
+    @staticmethod
+    def list_to_ll(l: list) -> Optional[ListNode]:
+
+        ll_head = None
+        ll_curr = None
+        for c in l:
+            new = ListNode(c)
+
+            if not ll_head:
+                ll_head = new
+                ll_curr = new
+                continue
+
+            ll_curr.next = new
+            ll_curr = new
+
+        return ll_head
+
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        l = self.ll_to_list(head)
+        l = list({k:v for k,v in collections.Counter(l).items() if v == 1}.keys())
+        return Solution.list_to_ll(l)
