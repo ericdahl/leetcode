c433e10f7ebbe9794a4d2fd3fd7634f6752d74cc
Eric Dahl <eric.c.dahl@gmail.com>
2022-09-05 11:36:14 -0700

    python: 219 simplify

--- python3/p219.py
+++ python3/p219.py
@@ -1,12 +1,12 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
         positions = defaultdict(list)
 
         for index, value in enumerate(nums):
             positions[value].append(index)
 
-        for p in [l for l in positions.values() if len(l) >= 2]:
-
+        for p in positions.values():
             for i in range(len(p)):
                 for j in range(i + 1, len(p)):
                     if p[j] - p[i] <= k:
