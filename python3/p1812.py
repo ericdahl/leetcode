559b5593b08f5a6c297dfc264a5147e8d020d6a5
Eric Dahl <eric.c.dahl@gmail.com>
2023-12-31 09:56:26 -0800

    python: add 1812

---
+++ python3/p1812.py
@@ -0,0 +1,15 @@
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        
+        m = {
+            "a": 1,
+            "b": 2,
+            "c": 3,
+            "d": 4,
+            "e": 5,
+            "f": 6,
+            "g": 7,
+            "h": 8,
+        }
+
+        return (m[coordinates[0]] + int(coordinates[1])) % 2 == 1
