fc6d22c6a55a2ab17239018b9c03abd17c8b4577
Eric Dahl <eric.c.dahl@gmail.com>
2024-03-14 10:12:20 -0700

    python: add 1603

---
+++ python3/p1603.py
@@ -0,0 +1,15 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.available = [None, big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.available[carType] <= 0:
+            return False
+        self.available[carType] -= 1
+        return True
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
