c956a7b1755189619d950157002bc479cf46644f
Eric Dahl <eric.c.dahl@gmail.com>
2024-05-18 10:23:01 -0700

    python: optimize 1828

--- python3/p1828.py
+++ python3/p1828.py
@@ -1,15 +1,11 @@
 class Solution:
 
-    @staticmethod
-    def distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-
     def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
         
-        for circle in queries:
-            circle.append(0) # add 4th item to keep track of # of matches
-            for point in points:
-                if Solution.distance(circle[0], circle[1], point[0], point[1]) <= circle[2]:
-                    circle[3] += 1
+        for c in queries:
+            c.append(0) # add 4th item to keep track of # of matches
+            for p in points:
+                if (c[0] - p[0])**2 + (c[1] - p[1])**2 <= c[2]**2:
+                    c[3] += 1
 
         return [c[3] for c in queries]
