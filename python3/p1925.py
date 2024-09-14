1391fa5d022dbe47a8433eea8e42431a8937157e
Eric Dahl <eric.c.dahl@gmail.com>
2024-09-14 10:51:49 -0700

    python: optimize 1925

--- python3/p1925.py
+++ python3/p1925.py
@@ -1,10 +1,9 @@
 class Solution:
     def countTriples(self, n: int) -> int:
+        squares = set(i * i for i in range(1, n + 1))
         count = 0
-        for a in range(1, n - 1):
-            for b in range(a + 1, n):
-                for c in range(b + 1, n + 1):
-                    if a**2 + b**2 == c**2:
-                        count += 2
-
+        for a in range(1, n + 1):
+            for b in range(a, n + 1):
+                if a**2 + b**2 in squares:
+                    count += 2
         return count
