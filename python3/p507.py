7f1efc0a4aad7519da385eb1c9d47551a89f4fa2
Eric Dahl <eric.c.dahl@gmail.com>
2024-11-23 12:18:08 -0800

    python: cleanup 507

--- python3/p507.py
+++ python3/p507.py
@@ -1,20 +1,18 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
 
-        if num == 1:
-            return False
-
-
         s = 1
-        max_num = num
-        i = 2
-        while i < max_num:
+        sqrt_num = int(math.sqrt(num))
+
+        for i in range(2, sqrt_num + 1):
             if num % i == 0:
                 s += i + num // i
 
-            if s > num:
-                return False
-            max_num = num // i
-            i += 1
-        return s == num
+                if s > num:
+                    return False
 
+        # special case - for perfect squares, don't count it twice
+        if sqrt_num * sqrt_num == num:
+            s -= sqrt_num
+
+        return s == num
