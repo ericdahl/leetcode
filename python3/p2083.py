8d0bf41e1cd07465ad743cbb251d63203e657116
Eric Dahl <eric.c.dahl@gmail.com>
2024-12-07 10:17:29 -0800

    python: optimize 2083

--- python3/p2083.py
+++ python3/p2083.py
@@ -1,8 +1,11 @@
 class Solution:
     def numOfPairs(self, nums: List[str], target: str) -> int:
         count = 0
-        for (a, b) in permutations(nums, 2):
-            if a + b == target:
+        target_len = len(target)
+        nums_len = [(len(n), n) for n in nums]
+        for ((a_len, a), (b_len, b)) in permutations(nums_len, 2):
+
+            if a_len + b_len == target_len and a + b == target:
                 count += 1
 
         return count
