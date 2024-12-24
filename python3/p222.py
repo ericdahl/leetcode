cc4145437c611efa86af81eef0fbf9c21a6ef709
Eric Dahl <eric.c.dahl@gmail.com>
2024-12-24 10:07:39 -0800

    python: optimize 222

--- python3/p222.py
+++ python3/p222.py
@@ -5,8 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+    @staticmethod
+    def height(node: Optional[TreeNode]) -> int:
+        height = 0
+        while node:
+            height += 1
+            node = node.left
+        return height
+
     def countNodes(self, root: Optional[TreeNode]) -> int:
 
         if not root:
             return 0
-        return 1 + Solution.countNodes(self, root.left) + Solution.countNodes(self, root.right)
+
+        left_height = Solution.height(root.left)
+        right_height = Solution.height(root.right)
+
+        print(f"left_height={left_height} / right_height={right_height}")
+
+        # if height of left subtree is the height of right subtree then
+        # left must be fully filled in (perfect) so left subtree has 2**left_height
+        # nodes, then add in the right side
+        if left_height == right_height:
+            return 2**left_height + self.countNodes(root.right)
+        else:
+            return 2**right_height + self.countNodes(root.left)
