abad73bb83add8b1ac25850a4250dfe03e39c7ce
Eric Dahl <eric.c.dahl@gmail.com>
2025-01-01 10:52:03 -0800

    python: optimize 3222

--- python3/p3222.py
+++ python3/p3222.py
@@ -1,14 +1,7 @@
 class Solution:
     def winningPlayer(self, x: int, y: int) -> str:
 
-        alice_turn = True
-
-        # valid move: 75 + 4 * 10
-        while x >= 1 and y >= 4:
-            x -= 1
-            y -= 4
-            alice_turn = not alice_turn
-
-        if alice_turn:
-            return "Bob"
-        return "Alice"
+        turns = min(x, y // 4)
+        if turns % 2 == 1:
+            return "Alice"
+        return "Bob"
