6731fbd5391be04abd6d14053e7e1c40294d95f1
Eric Dahl <eric.c.dahl@gmail.com>
2025-04-06 10:14:20 -0700

    python: simplify 704

--- python3/p704.py
+++ python3/p704.py
@@ -1,27 +1,14 @@
 class Solution:
-
     def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
 
-        # def search_rec
-        if nums[0] == target:
-            return 0
-
-        left = 0
-        right = len(nums) - 1
-        mid = (right - left) // 2
-
-        while left + 1 <= right:
-            if target == nums[mid]:
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
                 return mid
-            if target == nums[right]:
-                return right
-            delta = (right - left) // 2
-            if delta == 0: return -1
-            if target > nums[mid]:
-                left = mid
-                mid += delta
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                right = mid
-                mid -= delta
+                right = mid - 1
 
         return -1
