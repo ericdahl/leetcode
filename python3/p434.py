4b8f2c8420e7788a27276fb9339788db9a8055a0
Eric Dahl <eric.c.dahl@gmail.com>
2025-07-18 08:57:24 -0700

    python: optimize 434

--- python3/p434.py
+++ python3/p434.py
@@ -1,14 +1,4 @@
 class Solution:
     def countSegments(self, s: str) -> int:
-
-        count = 0
-        last_space = True
-        for c in s:
-            if c == " ":
-                last_space = True
-            else:
-                if last_space:
-                    count += 1
-                last_space = False
-
-        return count
+        # no args means it'll split on any whitespace repeated (different algorithm than if sep is given)
+        return len(s.split())
