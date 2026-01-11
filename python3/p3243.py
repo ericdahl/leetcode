28274c249cfdbefae91a2c619c14f38ed3f21cb6
Eric Dahl <eric.c.dahl@gmail.com>
2026-01-11 09:31:43 -0800

    python: cleanup 3243

--- python3/p3243.py
+++ python3/p3243.py
@@ -1,42 +1,39 @@
 class NeighborSum:
 
     def __init__(self, grid: List[List[int]]):
-        self.grid = grid
-        self.rows = len(grid)
-        self.cols = len(grid[0])
+        rows = len(grid)
+        cols = len(grid[0])
+
+        # extend grid with 0 on each side to simplify logic
+        self.extended_grid = [[0] * (cols + 2)]
+        for row in grid:
+            self.extended_grid.append([0] + row + [0])
+        self.extended_grid.append([0] * (cols + 2))
+
         self.coordinates = {}
 
-        for r in range(self.rows):
-            for c in range(self.cols):
-                self.coordinates[grid[r][c]] = (r,c)
+        for r in range(rows):
+            for c in range(cols):
+                self.coordinates[grid[r][c]] = (r + 1, c + 1)
 
 
     def adjacentSum(self, value: int) -> int:
         (r, c) = self.coordinates[value]
         s = 0
-        if r > 0:
-            s += self.grid[r - 1][c]
-        if r < self.rows - 1:
-            s += self.grid[r + 1][c]
-        if c > 0:
-            s += self.grid[r][c - 1]
-        if c < self.cols - 1:
-            s += self.grid[r][c + 1]
+        s += self.extended_grid[r - 1][c]
+        s += self.extended_grid[r + 1][c]
+        s += self.extended_grid[r][c - 1]
+        s += self.extended_grid[r][c + 1]
         return s
-
 
 
     def diagonalSum(self, value: int) -> int:
         (r, c) = self.coordinates[value]
         s = 0
-        if r > 0 and c > 0:
-            s += self.grid[r - 1][c - 1]
-        if r > 0 and c < self.cols - 1:
-            s += self.grid[r - 1][c + 1]
-        if r < self.rows - 1 and c < self.cols - 1:
-            s += self.grid[r + 1][c + 1]
-        if r < self.rows - 1 and c > 0:
-            s += self.grid[r + 1][c - 1]
+        s += self.extended_grid[r - 1][c - 1]
+        s += self.extended_grid[r - 1][c + 1]
+        s += self.extended_grid[r + 1][c + 1]
+        s += self.extended_grid[r + 1][c - 1]
         return s
 
 
