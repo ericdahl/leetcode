e34c10c75cb72963809ffa0ea187d1a9e2a69a1d
Eric Dahl <eric.c.dahl@gmail.com>
2026-05-02 10:06:47 -0700

    python: optimize 2502

--- python3/p2502.py
+++ python3/p2502.py
@@ -2,6 +2,7 @@ class Allocator:
 
     def __init__(self, n: int):
         self.mem = [0] * n
+        self.mem_by_id = defaultdict(list)
 
     def allocate(self, size: int, mID: int) -> int:
         # note: mID is more like a label of the owner, not a pointer (e.g., malloc). mID can refer to multiple allocated blocks
@@ -17,6 +18,7 @@ class Allocator:
                     start = i - contiguous + 1
                     for j in range(start, i + 1):
                         self.mem[j] = mID
+                        self.mem_by_id[mID].append(j)
                     return start
             else:
                 contiguous = 0
@@ -27,10 +29,10 @@ class Allocator:
 
     def freeMemory(self, mID: int) -> int:
         freed = 0
-        for i in range(len(self.mem)):
-            if self.mem[i] == mID:
-                self.mem[i] = 0
-                freed += 1
+        for i in self.mem_by_id[mID]:
+            self.mem[i] = 0
+            freed += 1
+        del self.mem_by_id[mID]
 
         return freed
         
