ac4bef29dee8c7a315c0c6a99e9fa313dfc1293e
Eric Dahl <eric.c.dahl@gmail.com>
2026-05-23 09:13:30 -0700

    python3: optimize 62

--- python3/p62.py
+++ python3/p62.py
@@ -1,11 +1,4 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        # without cache, this is too slow - short-circuit repeated calls
-        @cache
-        def dp(r, c):
-            if r == 1 or c == 1:
-                return 1
-            return dp(r - 1, c) + dp(r, c - 1)
-        
-        return dp(m, n)
+        return comb(m + n - 2, m - 1)
